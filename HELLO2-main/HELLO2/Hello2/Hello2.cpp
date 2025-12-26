59b410948001b716d8c64da6e25a3125b727eedc
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-26 23:58:43 +0100

    Add files via upload

---
+++ HELLO2-main/HELLO2/Hello2/Hello2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+int main()
+{
+    string Name;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Як вас звуть? = ";
+    cin >> Name;
+    cout << "Доброго дня, " << Name << "!";
+}
